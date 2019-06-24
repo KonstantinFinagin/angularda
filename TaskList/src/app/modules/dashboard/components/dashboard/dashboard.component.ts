9e800497fc3d9d3834bb7db03c37931cefa17d0d
kfinagin <kfinagin@dataart.com>
2019-06-25 00:52:30 +0300

    used switch

--- TaskList/src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ TaskList/src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -33,7 +33,7 @@ export class DashboardComponent implements OnInit {
     private ticketService: TicketService,
     private projectService: ProjectService,
     private dialog: MatDialog) {
-    }
+  }
 
   ngOnInit() {
     this.loadProjects();
@@ -49,7 +49,7 @@ export class DashboardComponent implements OnInit {
     this.ticketStatuses = [];
     for (const s in TicketStatus) {
 
-      if (typeof(TicketStatus[s]) === 'number') {
+      if (typeof (TicketStatus[s]) === 'number') {
 
         const ticketStatusNum = parseInt(TicketStatus[s], 10);
         const ticketStatusString = s;
@@ -74,11 +74,13 @@ export class DashboardComponent implements OnInit {
 
   getConnectedIndexes = (i: number): number[] => {
 
-    if (i === TicketStatus.Open) { return [TicketStatus.Development]; }
-    if (i === TicketStatus.Development) { return [TicketStatus.Open, TicketStatus.ReadyForQA]; }
-    if (i === TicketStatus.ReadyForQA) { return [TicketStatus.Development, TicketStatus.Test]; }
-    if (i === TicketStatus.Test) { return [TicketStatus.Closed, TicketStatus.Development]; }
-    if (i === TicketStatus.Closed) { return [TicketStatus.Open, TicketStatus.Test]; }
+    switch (i) {
+      case TicketStatus.Open: return [TicketStatus.Development];
+      case TicketStatus.Development: return [TicketStatus.Open, TicketStatus.ReadyForQA];
+      case TicketStatus.ReadyForQA: return [TicketStatus.Development, TicketStatus.Test];
+      case TicketStatus.Test: return [TicketStatus.Closed, TicketStatus.Development];
+      case TicketStatus.Closed: return [TicketStatus.Open, TicketStatus.Test];
+    }
   }
 
   getConnectedTo(i: number) {
@@ -101,16 +103,16 @@ export class DashboardComponent implements OnInit {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else if (connected.indexOf(event.container.id) === -1) {
-        // do nothing
+      // do nothing
     } else {
       transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
 
       const ticket = event.item.data as Ticket;
       ticket.status = this.getIndexByName(event.container.id);
-      this.subscriptions[0] = this.ticketService.updateTicketStatus(ticket).subscribe(data => {});
+      this.subscriptions[0] = this.ticketService.updateTicketStatus(ticket).subscribe(data => { });
     }
   }
 
